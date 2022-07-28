7b7c6f5f5aa16e293f85885063366472b4043061
Alexey <alexey.polskiy@gmail.com>
2022-07-28 23:32:01 +0500

    Compliet program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+void PrintArray(string[] printArr)
+{
+    for (int i = 0; i < printArr.Length; i++)
+    {
+        System.Console.Write($"{printArr[i]} ");
+    }
+}
+
+string[] firstArr = { "day", "Mon", "-2", "Wednesday", "Th", "Friday", "Saturday" };
+
+int j = 0;
+string[] secondArr = new string[firstArr.Length];
+for (int i = 0; i < firstArr.Length; i++)
+{
+    string temp = firstArr[i];
+    if (firstArr[i].Length < 4)
+    {
+        secondArr[j] = temp;
+        j++;
+    }
+}
+
+PrintArray(secondArr);
